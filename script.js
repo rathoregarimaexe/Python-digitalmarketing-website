010f4f93c8880f86d379b3007c9ac2116c6c53a2
rathoregarimaexe <131789338+rathoregarimaexe@users.noreply.github.com>
2023-05-05 20:35:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("Welcome to Digicraft!");
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+const menuItems
